c9c6cf0f9333021727ba68e9f56ee117d2f48e6c
Aman Pratap Singh <113757487+aman1-2@users.noreply.github.com>
2023-09-06 10:53:40 +0530

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,24 @@
+/* This is our first prpgram Printing the iconic hello world program.*/
+public class helloWorld{
+    public static void main(String args[]){
+        System.out.print("Hello World -> by using print function."); //Stats the next line printing exact from the world where it ends
+
+        System.out.println("Aman Pratap Singh -> by using println function."); //Starts the next line printing from the new fresh line.
+
+        System.out.printf("Aman Pratap Singh -> by using printf function"); //A function similar to what we use in the our c-programming language to print the output.
+
+    }
+}
+
+/* So the first two lines of the program can be taken as boiler-plate code as these are the
+must to write in our programs..
+public class <file_name>{
+    public static void main(String args[]){
+
+    }
+}  we have our outer class name as file name because the java complier searches for the class
+name as its file name therfore it is preffred to have the outer class name similar to the file
+name...
+And the execution of the program starts from the main function that's why in the next line we
+have a main fucntion...
+*/
